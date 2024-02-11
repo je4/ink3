cee05dc45117be55c4c83f29db1c79423ce442cd
Juergen Enge <you@example.com>
2024-02-11 11:15:27 +0100

    detail_audio added

--- data/web/templates/embed.go
+++ data/web/templates/embed.go
@@ -2,5 +2,5 @@ package templates
 
 import "embed"
 
-//go:embed index.gohtml search_grid.gohtml head.gohtml nav.gohtml footer.gohtml detail_text.gotmpl detail.gohtml detail_image.gohtml detail_pdf.gohtml detail_video.gohtml zoom.gohtml
+//go:embed index.gohtml search_grid.gohtml head.gohtml nav.gohtml footer.gohtml detail_text.gotmpl detail.gohtml detail_image.gohtml detail_pdf.gohtml detail_video.gohtml detail_audio.gohtml zoom.gohtml
 var FS embed.FS
--- pkg/server/controller.go
+++ pkg/server/controller.go
@@ -839,7 +839,7 @@ func (ctrl *Controller) detail(c *gin.Context) {
 		query.Set("cursor", cursorString)
 	}
 	templateName := "detail.gohtml"
-	textTemplate, err := ctrl.loadHTMLTemplate(templateName, []string{"head.gohtml", "footer.gohtml", "nav.gohtml", "detail_image.gohtml", "detail_video.gohtml", "detail_pdf.gohtml", templateName})
+	textTemplate, err := ctrl.loadHTMLTemplate(templateName, []string{"head.gohtml", "footer.gohtml", "nav.gohtml", "detail_image.gohtml", "detail_video.gohtml", "detail_audio.gohtml", "detail_pdf.gohtml", templateName})
 	if err != nil {
 		ctrl.logger.Error().Err(err).Msgf("cannot load template '%s'", templateName)
 		c.AbortWithStatusJSON(http.StatusInternalServerError, fmt.Sprintf("cannot load template '%s': %v", templateName, err))
