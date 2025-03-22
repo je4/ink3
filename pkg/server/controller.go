d0357ed991432bd319b39ef737586153ab84d8bf
Juergen Enge <you@example.com>
2025-03-22 12:52:37 +0100

    mei tab

--- pkg/server/controller.go
+++ pkg/server/controller.go
@@ -1728,6 +1728,7 @@ func (ctrl *Controller) detail(c *gin.Context) {
 		"detail_video.gohtml",
 		"detail_audio.gohtml",
 		"detail_pdf_dflip.gohtml",
+		"detail_verovio.gohtml",
 		//"detail_pdf_pdfjs.gohtml",
 		//"detail_pdf_3dflipbook.gohtml",
 		templateName})
