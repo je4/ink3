13a2c7a57dd82ab646141d6b9cbccf59e6625a81
Juergen Enge <you@example.com>
2026-06-20 20:49:27 +0200

    lösche web daten

--- data/_web/ink3/static/js/chat.js
+++
@@ -1,9 +0,0 @@
-function chat(url, cursor) {
-    let search = document.getElementById("search").value;
-
-    const params = new URLSearchParams({
-        query: search,
-    });
-
-    window.location.href = url + "?" + params.toString();
-}
